20bf9aba1775ced891cd8d55ecac087dcb95dc8b
Arnav Sharma <arnav@uber.com>
2019-10-16 13:28:24 +0530

    Creating V3

--- v3/v3.go
+++ v3/v3.go
@@ -1,9 +1,22 @@
 package v3
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
 	"github.com/anshal21/ftp-server/operator"
 	"github.com/anshal21/ftp-server/v2"
 )
+
+const (
+	_filePathSeparator = string(filepath.Separator)
+
+	_fileDelimiter = "\n"
+)
+
 type v3 struct {
 	operator.Operator
 
@@ -19,3 +32,26 @@ func New() operator.Operator {
 	}
 }
 
+func (v v3) Get(fileName string) (io.Reader, error) {
+	if file, ok := v.fileCache[fileName]; ok {
+		v.cacheHit++
+		fmt.Printf("Info: Cache Hit For File: %v,Total  Cache Hit Count: %v\n", fileName, v.cacheHit)
+		return bytes.NewReader(file), nil
+	}
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	v.cacheMiss++
+	buff := make([]byte, 1024*1024)
+	n, err := f.Read(buff)
+
+	fmt.Printf("Info: Cache Miss For File: %v, Total Cache Miss Count: %v\n", fileName, v.cacheMiss)
+
+	v.fileCache[fileName] = buff[:n]
+
+	return bytes.NewReader(buff[:n]), nil
+
+}

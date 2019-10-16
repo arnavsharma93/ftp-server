0742d1450da9481623d653e63e22481ff5da4229
Arnav Sharma <arnav@uber.com>
2019-10-16 13:13:49 +0530

    Interface implementation

--- v1/v1.go
+++ v1/v1.go
@@ -1,5 +1,20 @@
 package v1
+import (
+	"io"
+)
+
 type v1 struct{}
 func New() operator.Operator {
 	return v1{}
+}
+
+func (v v1) List(dirName string) (io.Reader, error) {
+}
+
+func (v v1) Delete(dirName string) error {
+
+}
+
+func (v v1) Get(fileName string) (io.Reader, error) {
+	return nil, nil
 }

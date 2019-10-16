dd2404c650b706a2cc55140e3d0b35a71b4300a0
Arnav Sharma <arnav@uber.com>
2019-10-16 13:01:51 +0530

    Import, constants, variables

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+)
+
+const (
+	_operationURLParameterKey = "operation"
+	_dirURLParamterKey        = "dir"
+
+	_FTPOperationList   = "ls"
+	_FTPOperationDelete = "del"
+	_FTPOperationGet    = "get"
+
+	_serverPort = ":8000"
+)
+
+var (
+	helpMessage = fmt.Sprintf(`Please provide one the following option
+  - %v: list down the files in a directory
+  - %v: delete files in a directory
+  - %v: returns a file
+`, _FTPOperationList, _FTPOperationDelete, _FTPOperationGet)
+)
+

37a3d0c38eabc23659b5057c686d76c6b89c2d17
Arnav Sharma <arnav@uber.com>
2019-10-16 13:15:41 +0530

    For loop

--- v1/v1.go
+++ v1/v1.go
@@ -1,18 +1,59 @@
 package v1
+
 import (
+	"bytes"
 	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/anshal21/ftp-server/operator"
 )
 
 type v1 struct{}
+
+const (
+	_fileDelimiter = "\n"
+
+	_filePathSeparator = string(filepath.Separator)
+)
+
 func New() operator.Operator {
 	return v1{}
 }
 
 func (v v1) List(dirName string) (io.Reader, error) {
+	dir, _ := os.Open(dirName)
+	files, _ := dir.Readdir(0)
+
+	fileNames := make([]string, 0)
+
+	for _, file := range files {
+		fileNames = append(fileNames, file.Name())
+	}
+
+	output := strings.Join(fileNames, _fileDelimiter)
+	return bytes.NewReader([]byte(output)), nil
 }
 
 func (v v1) Delete(dirName string) error {
 
+	dir, _ := os.Open(dirName)
+	files, _ := dir.Readdir(0)
+
+	for _, file := range files {
+		filePath := strings.Join([]string{dirName, file.Name()}, _filePathSeparator)
+		if file.IsDir() {
+			v.Delete(filePath)
+		} else {
+			os.Remove(filePath)
+		}
+
+	}
+
+	os.Remove(dirName)
+
+	return nil
 }
 
 func (v v1) Get(fileName string) (io.Reader, error) {

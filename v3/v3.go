1e1b7e1002c6cb7352f50c8ffefb0047aa83770d
Arnav Sharma <arnav@uber.com>
2019-10-16 13:27:56 +0530

    Embedding

--- v3/v3.go
+++ v3/v3.go
@@ -1,1 +1,21 @@
 package v3
+
+import (
+	"github.com/anshal21/ftp-server/operator"
+	"github.com/anshal21/ftp-server/v2"
+)
+type v3 struct {
+	operator.Operator
+
+	fileCache map[string][]byte
+	cacheHit  int
+	cacheMiss int
+}
+
+func New() operator.Operator {
+	return v3{
+		Operator:  v2.New(),
+		fileCache: make(map[string][]byte),
+	}
+}
+
